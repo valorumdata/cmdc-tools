b4732ad20eefff9e146f582463df151de66086f0
Chase <cc7768@gmail.com>
2020-05-09 18:03:11 -0400

    Add class structure and inherit on_conflict...

--- src/cmdc/datasets/wei/data.py
+++ src/cmdc/datasets/wei/data.py
@@ -3,9 +3,15 @@ import os
 import pandas as pd
 import pickle
 
+from cmdc.datasets import OnConflictNothingBase
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+
+
+# Default values for credential and token files
+CRED_FILE = "./conf/local/google_credentials.json"
+TOKEN_FILE = "./conf/local/google_token.pickle"
 
 
 def create_gdrive_service(cred_fn="credentials.json", token_fn="token.pickle"):
@@ -79,25 +85,67 @@ def retrieve_spreadsheet(service, fileId):
     return out
 
 
-def update_WEI():
+class WEI(OnConflictNothingBase):
     """
-    Fetches and updates the weekly economic index published by Jim
-    Stock on his website at https://www.jimstock.org/
+    The weekly economic index (WEI) is an index developed and
+    published by Jim Stock. It can be found online on his blog at
+    https://www.jimstock.org/ --- The description of the data
+    that follows comes from his blog.
 
-    Returns
-    -------
-    wei : pd.DataFrame
-        A DataFrame that contains the weekly economic index
+    The WEI is a composite of 6 weekly economic indicators:
+      * Redbook same-store sales
+      * Rasmussen Consumer Confidence
+      * new claims for unemployment insurance
+      * the American Staffing Association Staffing Index
+      * steel production
+      * wholesale sales of gasoline, diesel, and jet fuel
+
+    All series are represented as year-over-year percentage changes.
+    These series are combined into a single index of weekly economic
+    activity. The index closely tracks monthly industrial production
+    and quarterly GDP. Of these six series, five are available by
+    Thursday morning for the week ending the previous Saturday.
     """
-    service = create_gdrive_service(
-        cred_fn="./conf/local/google_credentials.json",
-        token_fn="./conf/local/google_token.pickle"
-    )
-    wei_file = retrieve_spreadsheet(service, "192MTTC1Tqol_LLgF-00R7-2c8jel-QmV")
-    wei = pd.read_excel(wei_file)
+    fileId = "192MTTC1Tqol_LLgF-00R7-2c8jel-QmV"
+    pk = '("date")'
+    table_name = "wei"
 
-    wei = wei.rename(columns={"Date": "date", "WEI": "wei"})
-    wei["country"] = "US"
+    def __init__(self, cred_fn=CRED_FILE, token_fn=TOKEN_FILE):
+        self.cred_fn = cred_fn
+        self.token_fn = token_fn
 
-    return wei
+        return None
+
+    def get(self):
+        """
+        Fetches and updates the weekly economic index published by Jim
+        Stock on his website at https://www.jimstock.org/
+
+        Returns
+        -------
+        wei : pd.DataFrame
+            A DataFrame that contains the weekly economic index
+        """
+
+        # Create google drive api service
+        service = create_gdrive_service(
+            cred_fn=self.cred_fn,
+            token_fn=self.token_fn
+        )
+
+        # Fetch file as bytesIO and then read into pandas
+        wei_file = retrieve_spreadsheet(service, self.fileId)
+        wei = pd.read_excel(wei_file)
+
+        # Rename columns and set country value in case we add other
+        # countries in the future
+        wei = wei.rename(columns={"Date": "date", "WEI": "wei"})
+        wei["country"] = "US"
+
+        # Set index values so that we have primary key set
+        wei = wei.set_index("date")
+
+        self.df = wei
+
+        return wei
 
