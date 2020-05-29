f0981fbdcfcdcb6437dfeb3b76f8917e047e1d8f
Chase <cc7768@gmail.com>
2020-05-29 01:46:00 -0400

    Update code/schema to include suspected/confirmed cases

--- schemas/covid_data.sql
+++ schemas/covid_data.sql
@@ -4,23 +4,62 @@ CREATE TABLE meta.covid_variables (
     name text
 );
 
+COMMENT ON TABLE meta.covid_variables IS E'This table contains a list of variables and their corresponding identifiers used in `data.us_covid`. The variables include:
+
+* cases_suspected: The number of cases that are suspected, but not confirmed, to have COVID in the geography
+* cases_confirmed: The number of cases that are confirmed to have COVID in the geography
+* cases_total: Total number of cases, both confirmed and suspected, in the geography
+* deaths_suspected: The total number of deaths that are suspected to have been caused, but not confirmed to have been caused, by COVID in the geography
+* deaths_confirmed: Total number of deaths that have been confirmed to have been caused by COVID in the geography
+* deaths_total: Total number of deaths including both suspected and confirmed
+* positive_tests_total: The total number of tests that have returned positive for COVID
+* negative_tests_total: The total number of tests that have returned negative for COVID
+* icu_beds_capacity_count: The number of ICU beds available in a particular geography
+* icu_beds_in_use_any: The number of ICU beds currently occupied by a patient
+* icu_beds_in_use_covid_suspected: The number of ICU beds occupied by a patient that is suspected to have COVID
+* icu_beds_in_use_covid_confirmed: The number of ICU beds occupied by a patient that is confirmed to have COVID
+* icu_beds_in_use_covid_total: The total number of ICU beds occupied by a patient that is suspected or confirmed to have COVID
+* icu_beds_in_use_covid_new: The number of new patients admitted to the ICU that are suspected or confirmed to have COVID
+* hospital_beds_capacity_count: The number of hospital beds available in a particular geography
+* hospital_beds_in_use_any: The number of hospital beds currently occupied by a patient
+* hospital_beds_in_use_covid_suspected: The number of hospital beds occupied by a patient that is suspected to have COVID
+* hospital_beds_in_use_covid_confirmed: The number of hospital beds occupied by a patient that is confirmed to have COVID
+* hospital_beds_in_use_covid_total: The total number of hospital beds occupied by a patient that is suspected or confirmed to have COVID
+* hospital_beds_in_use_covid_new: The number of new patients admitted to the hospital that are suspected or confirmed to have COVID
+* ventilators_capacity_count: The number of patients that can be supported by the geographies current ventilators
+* ventilators_in_use_any: The number of patients currently being supported by a ventilator
+* ventilators_in_use_covid_suspected: The number of patients that are suspected of having COVID that are being supported by a ventilator
+* ventilators_in_use_covid_confirmed: The number of patients that are confirmed to have COVID that are being supported by a ventilator
+* ventilators_in_use_covid_total: The total number of patients being supported by a ventilator who are confirmed or suspected of having COVID
+* ventilators_in_use_covid_new: The number of new patients who have been put on a ventilator
+';
+
+COMMENT ON COLUMN data.us_covid.id is E'The variable id used in `data.us_covid`';
+COMMENT ON COLUMN data.us_covid.name is E'The variable name as described in the table description';
+
 INSERT INTO meta.covid_variables (name) VALUES
-('cases_total'),
-('deaths_total'),
-('positive_tests_total'),
-('negative_tests_total'),
-('icu_beds_capacity_count'),
-('icu_beds_in_use_any'),
-('icu_beds_in_use_covid'),
-('icu_beds_in_use_covid_new'),
-('hospital_beds_capacity_count'),
-('hospital_beds_in_use_any'),
-('hospital_beds_in_use_covid'),
-('hospital_beds_in_use_covid_new'),
-('ventilators_capacity_count'),
-('ventilators_in_use'),
-('ventilators_in_use_covid'),
-('ventilators_in_use_covid_new');
+  ('cases_total'),
+  ('deaths_total'),
+  ('positive_tests_total'),
+  ('negative_tests_total'),
+  ('icu_beds_capacity_count'),
+  ('icu_beds_in_use_any'),
+  ('icu_beds_in_use_covid_suspected'),
+  ('icu_beds_in_use_covid_confirmed'),
+  ('icu_beds_in_use_covid_total'),
+  ('icu_beds_in_use_covid_new'),
+  ('hospital_beds_capacity_count'),
+  ('hospital_beds_in_use_any'),
+  ('hospital_beds_in_use_covid_suspected'),
+  ('hospital_beds_in_use_covid_confirmed'),
+  ('hospital_beds_in_use_covid_total'),
+  ('hospital_beds_in_use_covid_new'),
+  ('ventilators_capacity_count'),
+  ('ventilators_in_use_any'),
+  ('ventilators_in_use_covid_suspected'),
+  ('ventilators_in_use_covid_confirmed'),
+  ('ventilators_in_use_covid_total'),
+  ('ventilators_in_use_covid_new');
 
 DROP TABLE IF EXISTS data.covid;
 
@@ -38,23 +77,6 @@ COMMENT ON TABLE data.us_covid IS E'Contains key count data for tracking COVID-1
 COMMENT ON COLUMN data.us_covid.vintage is E'The date when the data was accessed';
 COMMENT ON COLUMN data.us_covid.dt is E'The date for which the data corresponds';
 COMMENT ON COLUMN data.us_covid.fips is E'The 2 (5) digit fips code identifying us state (county)';
-COMMENT ON COLUMN data.us_covid.variable_id is E'The id of the variable in this row. See table `meta.covid_variables` for more information. Variables can include:
-
-- `cases_total`
-- `deaths_total`
-- `positive_tests_total`
-- `negative_tests_total`
-- `icu_beds_capacity_count`
-- `icu_beds_in_use_any`
-- `icu_beds_in_use_covid`
-- `icu_beds_in_use_covid_new`
-- `hospital_beds_capacity_count`
-- `hospital_beds_in_use_any`
-- `hospital_beds_in_use_covid`
-- `hospital_beds_in_use_covid_new`
-- `ventilators_capacity_count`
-- `ventilators_in_use`
-- `ventilators_in_use_covid`
-- `ventilators_in_use_covid_new`
+COMMENT ON COLUMN data.us_covid.variable_id is E'The id of the variable in this row. See table `meta.covid_variables` for more information.
 ';
 COMMENT ON COLUMN data.us_covid.value is E'The value of the variable for the given region, on a date, and a vintage';
--- src/cmdc/datasets/official/CA/data.py
+++ src/cmdc/datasets/official/CA/data.py
@@ -8,6 +8,17 @@ CA_COUNTY_URL = "https://data.chhs.ca.gov/"
 CA_COUNTY_URL += "dataset/6882c390-b2d7-4b9a-aefa-2068cee63e47/resource/"
 CA_COUNTY_URL += "6cd8d424-dfaa-4bdd-9410-a3d656e1176e/download/covid19data.csv"
 
+C_RENAMER = {
+    "County Name": "name",
+    "Most Recent Date": "dt",
+    "Total Count Confirmed": "cases_total",
+    "Total Count Deaths": "deaths_total",
+    "COVID-19 Positive Patients": "hospital_beds_in_use_covid_confirmed",
+    "Suspected COVID-19 Positive Patients": "hospital_beds_in_use_covid_suspected",
+    "ICU COVID-19 Positive Patients": "icu_beds_in_use_covid_confirmed",
+    "ICU COVID-19 Suspected Patients": "icu_beds_in_use_covid_suspected",
+}
+
 
 class CACountyData(OnConflictNothingBase):
     table_name = "us_covid"
@@ -31,19 +42,19 @@ class CACountyData(OnConflictNothingBase):
         return textwrap.dedent(out)
 
     def get(self):
-        crenamer = {
-            "County Name": "name",
-            "Most Recent Date": "dt",
-            "Total Count Confirmed": "cases_total",
-            "Total Count Deaths": "deaths_total",
-            "COVID-19 Positive Patients": "hospital_beds_in_use_covid",
-            "ICU COVID-19 Positive Patients": "icu_beds_in_use_covid"
-        }
 
         df = pd.read_csv(CA_COUNTY_URL)
-        df = df.rename(columns=crenamer).loc[:, list(crenamer.values())]
+        df = df.rename(columns=C_RENAMER).loc[:, list(C_RENAMER.values())]
         df["dt"] = pd.to_datetime(df["dt"])
         df["vintage"] = pd.datetime.today()
+
+        # Create totals
+        df["hospital_beds_in_use_covid_total"] = df.eval(
+            "hospital_beds_in_use_covid_confirmed + hospital_beds_in_use_covid_suspected"
+        )
+        df["icu_beds_in_use_covid_total"] = df.eval(
+            "icu_beds_in_use_covid_confirmed + icu_beds_in_use_covid_suspected"
+        )
 
         df = df.melt(
             id_vars=["vintage", "dt", "name"],
