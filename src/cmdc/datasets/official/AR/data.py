3f2f182f59de1816909d59d97e2ca18c126d24eb
Trevor Lyon <trevor.lyon@icloud.com>
2020-06-03 13:49:08 -0600

    return df for arkansas

--- src/cmdc/datasets/official/AR/data.py
+++ src/cmdc/datasets/official/AR/data.py
@@ -2,4 +2,4 @@ from cmdc.datasets.official.arcgis import Arcgis
 
 
 def DataForArkansas():
-   return Arcgis(resource_id="PwY9ZuZRDiI5nXUB", dashboard_name="ADH_COVID19_Positive_Test_Results", server_id="")
+   return Arcgis(resource_id="PwY9ZuZRDiI5nXUB", dashboard_name="ADH_COVID19_Positive_Test_Results", server_id="").df()
