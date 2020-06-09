9d3e0e99e4787bcd8603d322e4395c058a3146f8
Chase <cc7768@gmail.com>
2020-06-09 13:51:38 -0400

    Add stuff to __init__.

--- src/cmdc_tools/datasets/official/__init__.py
+++ src/cmdc_tools/datasets/official/__init__.py
@@ -1,4 +1,6 @@
 from .base import ArcGIS, CountyData
 from .AK import Alaska
+from .AR import Arkansas
 from .CA import LA, CACountyData, Imperial, SanDiego
 from .MD import Maryland
+from .MA import Massachusetts
