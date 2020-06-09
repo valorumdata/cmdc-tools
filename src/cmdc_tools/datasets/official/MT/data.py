c5b06780980a37ea3289ec4e92fec06dc2f376f4
Chase <cc7768@gmail.com>
2020-06-08 23:04:58 -0400

    Update file and add insert statement

--- src/cmdc_tools/datasets/official/MT/data.py
+++ src/cmdc_tools/datasets/official/MT/data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import requests
+import textwrap
 
-from ...base import DatasetBaseNoDate
 from ..base import ArcGIS
+from ...base import DatasetBaseNoDate
 
 
 class Montana(ArcGIS, DatasetBaseNoDate):
@@ -10,14 +11,27 @@ class Montana(ArcGIS, DatasetBaseNoDate):
         self.ARCGIS_ID = "qnjIrwR8z5Izc0ij"
 
         if params is None:
-            self.params = {
+            params = {
                 "f": "json",
                 "where": "1=1",
                 "outFields": "*",
                 "returnGeometry": "false",
             }
 
-        super().__init__(params=params)
+        super().__init__(params)
+
+    def _insert_query(self, df, table_name, temp_name, pk):
+        out = f"""
+        INSERT INTO data.{table_name} (vintage, dt, fips, variable_id, value)
+        SELECT tt.vintage, tt.dt, us.fips, mv.id as variable_id, tt.value
+        FROM {temp_name} tt
+        LEFT JOIN meta.us_fips us ON tt.county=us.name
+        LEFT JOIN meta.covid_variables mv ON tt.variable_name=mv.name
+        WHERE us.fips > 30000 AND us.fips < 31000
+        ON CONFLICT {pk} DO NOTHING
+        """
+
+        return textwrap.dedent(out)
 
     def get(self):
         dt = pd.Timestamp.today().normalize()
@@ -32,10 +46,12 @@ class Montana(ArcGIS, DatasetBaseNoDate):
         main = pd.DataFrame.from_records(
             [x["attributes"] for x in res.json()["features"]]
         )
+        main = main.rename(columns={"County": "county"})
+
         # get cases
         case_outcome = self.get_cases(main)
         # get hospitalzation
-        hbiuc = self.get_hospitalization_stats(main)        
+        hbiuc = self.get_hospitalization_stats(main)
 
         return(
             case_outcome
@@ -44,35 +60,35 @@ class Montana(ArcGIS, DatasetBaseNoDate):
             # Reset index
             .reset_index()
             # Melt
-            .melt(id_vars=["County"], var_name="variable_name")
+            .melt(id_vars=["county"], var_name="variable_name")
             # Normalize
             .assign(vintage=dt, dt=dt)
         )
-        
+
     def get_cases(self, df):
-    
+
         case_outcome = (
-            df.groupby(["County", "Outcome"])["Sex"]
+            df.groupby(["county", "Outcome"])["Sex"]
             .count()
             .unstack(level="Outcome")
             .fillna(0)
             .astype(int)
             .rename(columns={
-                "Active":"active_total", 
+                "Active":"active_total",
                 "Deceased":"deaths_total",
                 "Recovered":"recovered_total"
             })
-        )  
+        )
 
         return case_outcome
 
     def get_hospitalization_stats(self, df):
         hbiuc = (
-            df.groupby(["County", "Hospitalization"])["Sex"]
+            df.groupby(["county", "Hospitalization"])["Sex"]
             .count()
             .unstack(level="Hospitalization")
             .fillna(0)
             .astype(int)["Y"]
-            .rename("hospital_beds_in_use_covid_total")  
+            .rename("hospital_beds_in_use_covid_total")
         )
         return hbiuc
