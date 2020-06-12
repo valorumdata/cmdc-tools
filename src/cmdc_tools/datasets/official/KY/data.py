13498239f530f5ee4697312dce63b813628cdae6
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-12 14:22:14 -0400

    STY: clean style in KY

--- src/cmdc_tools/datasets/official/KY/data.py
+++ src/cmdc_tools/datasets/official/KY/data.py
@@ -11,37 +11,23 @@ class Kentucky(ArcGIS, DatasetBaseNoDate):
         out = f"https://kygisserver.ky.gov/arcgis/rest/services/WGS84WM_Services/{service}/FeatureServer/{sheet}/query"
         return out
 
-    def get(self):
+    def get(self) -> pd.DataFrame:
         df = self.get_all_sheet_to_df(
-            service="Ky_Cnty_COVID19_Cases_WGS84WM", 
-            sheet=0, 
-            srvid=""
+            service="Ky_Cnty_COVID19_Cases_WGS84WM", sheet=0, srvid=""
         )
 
-        renamed = df.rename(columns={
+        column_map = {
             "County": "county",
             "FIPS": "fips",
             "Hospitalized": "hospital_beds_in_use_covid_confirmed",
             "ICU": "icu_beds_in_use_covid_confirmed",
             "Deceased": "deaths_confirmed",
-            "Confirmed": "cases_confirmed"
-        })
+            "Confirmed": "cases_confirmed",
+        }
 
-        renamed = renamed[[
-            "county",
-            "fips",
-            "hospital_beds_in_use_covid_confirmed",
-            "icu_beds_in_use_covid_confirmed",
-            "deaths_confirmed",
-            "cases_confirmed"
-        ]]
+        renamed = df.rename(columns=column_map).loc[:, list(column_map.values())]
 
-        return (
-            renamed
-            .melt(
-                id_vars=["county", "fips"],
-                var_name="variable_name",
-                value_name="value"
-            )
-            .assign(vintage=pd.Timestamp.utcnow().normalize(), dt=pd.Timestamp.utcnow().normalize())
-        )
+        dt = pd.Timestamp.utcnow().normalize()
+        return renamed.melt(
+            id_vars=["county", "fips"], var_name="variable_name", value_name="value"
+        ).assign(vintage=dt, dt=dt)
