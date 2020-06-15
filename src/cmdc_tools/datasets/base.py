1a259ce6fe61a9bb27f5810441181e65e5d8e142
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-15 18:12:22 -0400

    ENH: add covid source tracker feature

--- src/cmdc_tools/datasets/base.py
+++ src/cmdc_tools/datasets/base.py
@@ -1,4 +1,6 @@
 import random
+from typing import Optional
+
 from abc import ABC, abstractmethod
 
 from .db_util import TempTable
@@ -18,6 +20,78 @@ class DatasetBase:
 
     def put_prep(self, conn, df):
         pass
+
+    def log_covid_source(
+        self,
+        connstr: sa.engine.Engine,
+        df: pd.DataFrame,
+        source: str,
+        state_fips: Optional[int] = None,
+    ):
+        """
+        Log the source of all variables in the DataFrame df
+
+        :param conn: SQLAlchemy engine
+        :param df: The DataFrame added to the data.us_covid table
+        :param source: A string containing the source of the data
+        :param state_fips: If `df` recognizes locations by name lookup
+        from `county` column, the fips state code must be passed to
+        disambiguate the location code for the observation
+        :return:
+        """
+        # check for valid column names
+        # first look for fips
+        columns = list(df.columns)
+
+        if "fips" in df:
+            join_locs = "LEFT JOIN META.us_fips ff using(fips)"
+            loc = "fips"
+        elif "county" in df:
+            if state_fips is None:
+                msg = "Found county column, but state_fips not given. Must pass to correctly join"
+                raise ValueError(msg)
+            assert isinstance(state_fips, int)
+            assert state_fips < 100
+            fips_min = state_fips * 1000  # map from XX to XX000
+            fips_max = fips_min + 999  # map from XX000 to XX999
+
+            select = "SELECT name, fips from meta.us_fips where fips between {fips_min} and {fips_max}"
+            join_locs = f"LEFT JOIN ({select}) ff on ff.name = tt.county"
+            loc = "county"
+        else:
+            raise ValueError("Expected either `fips` or `county` in `df`")
+
+        if "variable_name" in df:
+            var = "variable_name"
+            join_covid = (
+                "LEFT JOIN meta.covid_variables mv on mv.name = tt.variable_name"
+            )
+        elif "variable_id" in df:
+            var = "variable_id"
+            join_covid = "LEFT JOIN meta.covid_variables mv on mv.id = tt.variable_id"
+        else:
+            raise ValueError("Expected either `variable_name` or `variable_id` in `df`")
+
+        temp_table = "__covid_sources_{}".format(str(random.randint(1000, 9999)))
+        sql = f"""
+        INSERT INTO data.covid_sources(date_accessed, location, variable_id, source)
+        SELECT tt.this_date, ff.fips, mv.id, tt.src
+        from {temp_table} tt
+        {join_locs}
+        {join_covid}
+        ON CONFLICT (date_accessed, location, variable_id) DO UPDATE set source=EXCLUDED.source
+        """
+
+        this_date = pd.Timestamp.utcnow().normalize()
+
+        to_sql = (
+            df[[loc, var]].drop_duplicates().assign(this_date=this_date, src=source)
+        )
+
+        with sa.create_engine(connstr).connect() as conn:
+            kw = dict(temp=False, if_exists="replace", destroy=True)
+            with TempTable(to_sql, temp_table, conn, **kw):
+                conn.execute(sql)
 
 
 class DatasetBaseNoDate(DatasetBase, ABC):
