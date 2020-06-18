b64b4afd43e3d129c99fd6e8954b618223abb544
Chase <cc7768@gmail.com>
2020-06-18 00:48:00 -0400

    Update the score view to generate names correctly

--- schemas/score_views.sql
+++ schemas/score_views.sql
@@ -13,6 +13,8 @@ CREATE OR REPLACE VIEW public.scores AS
 	  SELECT location, value AS frac65p FROM api.demographics WHERE variable = 'Fraction of population over 65'
   ), daily_case_avg AS (
 	  SELECT fips, (MAX(value) - MIN(value))::REAL / 30 AS recent_cases FROM api.usafacts_covid WHERE dt > NOW() - INTERVAL '30 days' AND variable='cases_total' GROUP BY fips
+  ), states AS (
+      SELECT * FROM meta.us_fips WHERE fips < 100
   ), scores AS (
     SELECT
       demo_pop.location,
@@ -25,19 +27,30 @@ CREATE OR REPLACE VIEW public.scores AS
     LEFT JOIN daily_case_avg ON demo_pop.location=daily_case_avg.fips
     ORDER BY score DESC
   )
-  SELECT s.location, s.score
+  SELECT
+    s.location,
+    CASE
+      WHEN ucf.name=st.name THEN ucf.name
+    ELSE
+      ucf.name || ', ' || st.name
+    END
+    AS name,
+    s.score
   FROM scores s
+  LEFT JOIN meta.us_fips usf ON s.location=usf.fips
+  LEFT JOIN states st ON usf.state=st.state
 ;
 
+
 CREATE OR REPLACE VIEW public.state_scores AS
-  SELECT s.location, s.score
+  SELECT s.location, s.name, s.score
   FROM public.scores s
   WHERE s.location < 100
 ;
 
 
 CREATE OR REPLACE VIEW public.county_scores AS
-  SELECT s.location, s.score
+  SELECT s.location, s.name, s.score
   FROM public.scores s
   WHERE s.location > 1000
 ;
