b5b6576cde8b1108123f20e7f7e1fbaddd88f005
Trevor Lyon <trevor.lyon@icloud.com>
2020-06-24 14:50:30 -0600

    FEAT: New Mexico scraper

---
+++ src/cmdc_tools/datasets/official/NM/data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+
+from ...base import DatasetBaseNoDate
+
+
+class NewMexico(DatasetBaseNoDate):
+    def get(self):
+        url = "https://e7p503ngy5.execute-api.us-west-2.amazonaws.com/prod/GetCounties"
+        res = requests.get(url)
+
+        df = pd.DataFrame(res.json()["data"])
+
+        renamed = df.rename(
+            columns={
+                "name": "county",
+                "cases": "cases_total",
+                "deaths": "deaths_total",
+                "created": "dt",
+                "recovered": "recovered_total",
+            }
+        )
+        renamed["dt"] = renamed["dt"].map(lambda x: pd.datetime.fromtimestamp(x / 1000))
+        return renamed[
+            ["dt", "county", "cases_total", "deaths_total", "recovered_total",]
+        ].melt(id_vars=["dt", "county"], var_name="variable_name")
