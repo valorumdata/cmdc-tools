c4ffda5991981763d4569e9bd70f0c846804dfab
Trevor Lyon <trevor.lyon@icloud.com>
2020-06-24 14:17:15 -0600

    WIP: Iowa case numbers and hosp data

---
+++ src/cmdc_tools/datasets/official/IA/data.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+from ...base import DatasetBaseNoDate
+from ..base import ArcGIS
+
+
+class Iowa(ArcGIS, DatasetBaseNoDate):
+    ARCGIS_ID = "vPD5PVLI6sfkZ5E4"
+
+    def get(self):
+        pass
+
+    def _get_cases(self):
+        df = self.get_all_sheet_to_df("IA_COVID19_Cases", 0, "")
+
+        renamed = df.rename(
+            columns={
+                "FIPS": "fips",
+                "Confirmed": "cases_confirmed",
+                "Recovered": "recovered_total",
+                "Deaths": "deaths_total",
+                "last_updated": "dt",
+            }
+        )
+
+        renamed["dt"] = renamed["dt"].map(lambda x: pd.datetime.fromtimestamp(x / 1000))
+
+        return renamed[
+            ["dt", "fips", "cases_confirmed", "recovered_total", "deaths_total"]
+        ].sort_values(["dt", "fips"])
+
+    def _get_hosp(self):
+        df = self.get_all_sheet_to_df("COVID19_RMCC_Hospitalization", 0, "")
+        reanmed = df.rename(
+            columns={
+                "RMCC": "region",
+                "Total_Inpatient_Beds_Available": "hospital_beds_capacity_count",
+                "Total_Hospitalized": "hospital_beds_in_use_any",
+                "ICU": "icu_beds_in_use_any",
+                "Ventilators_Available": "ventilators_capacity_count",
+                "Ventilator": "ventilators_in_use_any",
+            }
+        )
+
+        renamed["dt"] = pd.Timestamp.utcnow().normalize()
+        return reanmed[
+            [
+                "dt",
+                "region",
+                "hospital_beds_capacity_count",
+                "hospital_beds_in_use_any",
+                "icu_beds_in_use_any",
+                "ventilators_capacity_count",
+                "ventilators_in_use_any",
+            ]
+        ]
