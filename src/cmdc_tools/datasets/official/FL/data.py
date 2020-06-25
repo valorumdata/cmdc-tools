3560f5a7d382319223e7d536847cf47eec4b73aa
Trevor Lyon <trevor.lyon@icloud.com>
2020-06-25 15:23:29 -0600

    remove self

--- src/cmdc_tools/datasets/official/FL/data.py
+++ src/cmdc_tools/datasets/official/FL/data.py
@@ -7,9 +7,9 @@ from ..base import ArcGIS
 
 class Florida(DatasetBaseNoDate, ArcGIS):
     ARCGIS_ID = "CY1LXxl9zlJeBuRZ"
-    self.state_fips = int(us.states.lookup("Florida").fips)
-    self.has_fips: bool = True
-    self.source = "https://fdoh.maps.arcgis.com/apps/opsdashboard/index.html#/8d0de33f260d444c852a615dc7837c86"
+    state_fips = int(us.states.lookup("Florida").fips)
+    has_fips: bool = True
+    source = "https://fdoh.maps.arcgis.com/apps/opsdashboard/index.html#/8d0de33f260d444c852a615dc7837c86"
 
     def get(self):
         df = self.get_all_sheet_to_df("Florida_COVID19_Cases_by_County_vw", 0, 1)
