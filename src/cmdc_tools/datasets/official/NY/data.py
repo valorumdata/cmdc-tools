e76261c019e492fb955b9ebf5705151e505ede0c
Chase Coleman <cc7768@gmail.com>
2020-06-29 18:12:09 -0400

    Small edits to ny data

--- src/cmdc_tools/datasets/official/NY/data.py
+++ src/cmdc_tools/datasets/official/NY/data.py
@@ -9,16 +9,24 @@ from ..base import CountyData
 class NewYork(DatasetBaseNoDate, CountyData):
     has_fips = False
     state_fips = int(us.states.lookup("New York").fips)
+    source = (
+        "https://covid19tracker.health.ny.gov/views/NYS-COVID19-Tracker"
+        "/NYSDOHCOVID-19Tracker-Map"
+    )
 
     def get(self):
         tests = self._get_tests_data()
-        return tests
+
+        out = pd.concat([tests], axis=0, ignore_index=True)
+        out["vintage"] = pd.Timestamp.now().normalize()
+
+        return out
 
     def _get_tests_data(self):
         tests_url = "https://health.data.ny.gov/api/views/xdss-u53e/rows.csv?accessType=DOWNLOAD"
 
-        df = pd.read_csv(tests_url, parse_dates=True)
-        renamed = df.rename(
+        df = pd.read_csv(tests_url, parse_dates=["Test Date"])
+        df = df.rename(
             columns={
                 "Test Date": "dt",
                 "County": "county",
@@ -29,12 +37,16 @@ class NewYork(DatasetBaseNoDate, CountyData):
             }
         )
 
-        return renamed[
-            [
-                "dt",
-                "county",
-                "tests_total",
-                "positive_tests_total",
-                # "negative_tests_total",
-            ]
-        ].melt(id_vars=["dt", "county"], var_name="variable_name")
+        keep = [
+            "dt",
+            "county",
+            "tests_total",
+            "positive_tests_total",
+            # "negative_tests_total",
+        ]
+
+        out = df.loc[:, keep].melt(
+            id_vars=["dt", "county"], var_name="variable_name"
+        )
+
+        return out
