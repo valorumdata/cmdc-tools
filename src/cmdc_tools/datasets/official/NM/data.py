002f679fed80058ac7bd84e97d93a13294b28b72
Spencer Lyon <spencerlyon2@gmail.com>
2020-07-02 12:54:31 -0400

    NM ready

--- src/cmdc_tools/datasets/official/NM/data.py
+++ src/cmdc_tools/datasets/official/NM/data.py
@@ -9,15 +9,12 @@ from .. import CountyData
 class NewMexico(DatasetBaseNoDate, CountyData):
     source = "https://cvprovider.nmhealth.org/public-dashboard.html"
     state_fips = int(us.states.lookup("NM").fips)
-    has_fips: bool = False
+    has_fips: bool = True
 
     def get(self):
-        county = self._get_county()
-        state = self._get_state()
-
-        result = pd.concat([county, state], sort=False)
-
-        return result.sort_values(["dt", "fips", "county"])
+        return pd.concat(
+            [self._get_county(), self._get_state()], ignore_index=True, sort=False
+        )
 
     def _get_county(self):
         url = "https://e7p503ngy5.execute-api.us-west-2.amazonaws.com/prod/GetCounties"
@@ -25,44 +22,40 @@ class NewMexico(DatasetBaseNoDate, CountyData):
 
         df = pd.DataFrame(res.json()["data"])
 
-        renamed = df.rename(
-            columns={
-                "name": "county",
-                "cases": "cases_total",
-                "deaths": "deaths_total",
-                "created": "dt",
-                "recovered": "recovered_total",
-            }
-        )
+        column_names = {
+            "countyId": "fips",
+            "cases": "cases_total",
+            "deaths": "deaths_total",
+            "created": "dt",
+            "recovered": "recovered_total",
+            "tests": "tests_total",
+        }
+
+        renamed = df.rename(columns=column_names)
+        renamed["fips"] = renamed["fips"] + self.state_fips * 1000
         renamed["dt"] = renamed["dt"].map(lambda x: pd.datetime.fromtimestamp(x / 1000))
-        return renamed[
-            ["dt", "county", "cases_total", "deaths_total", "recovered_total",]
-        ].melt(id_vars=["dt", "county"], var_name="variable_name")
+        return renamed.loc[:, list(column_names.values())].melt(
+            id_vars=["dt", "fips"], var_name="variable_name"
+        )
 
     def _get_state(self):
         url = "https://e7p503ngy5.execute-api.us-west-2.amazonaws.com/prod/GetPublicStatewideData"
         res = requests.get(url)
         df = pd.Series(res.json()["data"]).to_frame().T
 
-        renamed = df.rename(
-            columns={
-                "created": "dt",
-                "recovered": "recovered_total",
-                "currentHospitalizations": "hospital_beds_in_use_covid_total",
-                "deaths": "deaths_total",
-                "cases": "cases_total",
-            }
-        )
+        column_names = {
+            "created": "dt",
+            "recovered": "recovered_total",
+            "currentHospitalizations": "hospital_beds_in_use_covid_total",
+            "deaths": "deaths_total",
+            "cases": "cases_total",
+            "tests": "tests_total",
+        }
+
+        renamed = df.rename(columns=column_names)
         renamed["dt"] = renamed["dt"].map(lambda x: pd.datetime.fromtimestamp(x / 1000))
 
         renamed["fips"] = self.state_fips
-        return renamed[
-            [
-                "dt",
-                "fips",
-                "recovered_total",
-                "hospital_beds_in_use_covid_total",
-                "deaths_total",
-                "cases_total",
-            ]
-        ].melt(id_vars=["dt", "fips"], var_name="variable_name")
+        return renamed.loc[:, ["fips"] + list(column_names.values())].melt(
+            id_vars=["dt", "fips"], var_name="variable_name"
+        )
