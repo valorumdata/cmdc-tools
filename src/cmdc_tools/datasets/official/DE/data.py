65f1e3af0bd487d1982a6dc0b6735c8596eef2a5
Chase Coleman <cc7768@gmail.com>
2020-07-02 11:17:58 -0400

    Update DE code

--- src/cmdc_tools/datasets/official/DE/data.py
+++ src/cmdc_tools/datasets/official/DE/data.py
@@ -7,8 +7,8 @@ from ..base import CountyData
 
 
 class Delaware(DatasetBaseNoDate, CountyData):
+    has_fips = False
     state_fips = int(us.states.lookup("Delaware").fips)
-    has_fips: bool = False
     source_map = {
         "Sussex": "https://myhealthycommunity.dhss.delaware.gov/locations/county-sussex/download_covid_19_data",
         "Kent": "https://myhealthycommunity.dhss.delaware.gov/locations/county-kent/download_covid_19_data",
@@ -23,46 +23,42 @@ class Delaware(DatasetBaseNoDate, CountyData):
             df["county"] = county
             dfs.append(df)
 
-        return pd.concat(dfs, sort=False).melt(
-            id_vars=["dt", "county"], var_name="variable_name"
-        )
+        df = pd.concat(dfs, ignore_index=True, sort=False)
+        df["vintage"] = pd.Timestamp.utcnow().normalize()
+
+        return df
 
     def _get_from_source(self, source):
+        # Read data and set date
         df = pd.read_csv(source)
-
         df["dt"] = pd.to_datetime(df[["Year", "Month", "Day"]])
-        keep = df.loc[df.Unit == "people"][
-            ["Location", "County", "dt", "Statistic", "Value"]
-        ]
-        pivoted = (
-            keep.set_index(["Location", "County", "dt"])
-            .pivot(columns="Statistic")["Value"]
-            .reset_index()
-        )
-        renamed = pivoted.rename(
-            columns={
-                "County": "county",
-                "Confirmed Deaths": "deaths_confirmed",
-                "Cumulative Number of Confirmed Positive Cases": "cases_confirmed",
-                # 'Cumulative Number of Positive Cases': "cases_suspected",
-                "Deaths": "deaths_total",
-                "Cumulative Number of Probable Positive Cases": "cases_suspected",
-                "Recovered": "recovered_total",
-                "Tested Negative": "negative_tests_total",
-                "Total Persons Tested": "tests_total",
-            }
+
+        # Renamer for the variables that we want to keep
+        var_rename = {
+            "Deaths": "deaths_total",
+            "Cumulative Number of Positive Cases": "cases_total",
+            "Recovered": "recovered_total",
+            "Cumulative Number of Confirmed Positive Cases": "positive_tests_total",
+            "Tested Negative": "negative_tests_total",
+            "Total Persons Tested": "tests_total",
+        }
+
+        # We restrict to only totals (i.e., 'people') and only keep a subset of columns
+        df = (
+            df.query("Unit == 'people'")
+            .rename(
+                columns={
+                    "Location": "county",
+                    "Statistic": "variable_name",
+                    "Value": "value",
+                }
+            )
+            .loc[:, ["dt", "county", "variable_name", "value"]]
         )
 
-        return renamed[
-            [
-                "dt",
-                "deaths_confirmed",
-                "cases_confirmed",
-                "cases_suspected",
-                "deaths_total",
-                "cases_suspected",
-                "recovered_total",
-                "negative_tests_total",
-                # "tests_total",
-            ]
-        ]
+        # Rename variables and drop any variables that we haven't renamed
+        df["variable_name"] = df["variable_name"].replace(var_rename)
+        df = df.query("variable_name in @var_rename.values()")
+        df["value"] = df["value"].astype(int)
+
+        return df
