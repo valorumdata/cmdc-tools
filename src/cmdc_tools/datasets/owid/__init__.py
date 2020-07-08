e2cf37b83b5f336ea24e9136967eda3d36a6e0b2
Spencer Lyon <spencerlyon2@gmail.com>
2020-07-08 01:43:10 -0400

    ENH: add our world in data scraper

--- src/cmdc_tools/datasets/owid/__init__.py
+++ src/cmdc_tools/datasets/owid/__init__.py
@@ -35,19 +35,20 @@ class OWID(DatasetBaseNoDate):
         url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
         return (
             pd.read_csv(url, parse_dates=["date"])
-                .rename(columns={"date": "dt"})
+            .rename(columns={"date": "dt"})
+            .dropna(subset=["iso_code"])
         )
 
-    def _put_one(self, connstr: str, sub: pd.DataFrame, table_name: str, cols: List[str]):
+    def _put_one(
+        self, connstr: str, sub: pd.DataFrame, table_name: str, cols: List[str], pk: str
+    ):
         temp_name = f"__{table_name}" + str(random.randint(1000, 9999))
         sql = """
         INSERT INTO data.{table_name} ({cols})
         SELECT {cols} from {temp_name}
-        on conflict (iso_code) do nothing;
+        on conflict {pk} do nothing;
         """.format(
-            cols=",".join(cols),
-            temp_name=temp_name,
-            table_name=table_name
+            cols=",".join(cols), temp_name=temp_name, table_name=table_name, pk=pk
         )
 
         with sa.create_engine(connstr).connect() as conn:
@@ -57,13 +58,12 @@ class OWID(DatasetBaseNoDate):
 
     def put(self, conn: str, df: pd.DataFrame):
         # first handle locations
-        locations = (
-            df.groupby("iso_code")
-                .apply(lambda x: x.loc[:, meta_columns].iloc[0])
+        locations = df.groupby("iso_code").apply(
+            lambda x: x.loc[:, meta_columns].iloc[0]
         )
-        self._put_one(conn, locations, "owid_locations", meta_columns)
+        self._put_one(conn, locations, "owid_locations", meta_columns, pk="(iso_code)")
 
         # then handle data
-        data_columns = list(set(list(df)) - set(meta_columns))
-        data = df.loc[:, data_columns + ["iso_code"]]
-        self._put_one(conn, data, "owid_covid", data_columns)
+        data_columns = list(set(list(df)) - set(meta_columns)) + ["iso_code"]
+        data = df.loc[:, data_columns]
+        self._put_one(conn, data, "owid_covid", data_columns, pk="(iso_code, dt)")
