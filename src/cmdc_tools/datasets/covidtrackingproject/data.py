480e41f330ee12553aead541d2774e394112cd44
Spencer Lyon <spencerlyon2@gmail.com>
2020-07-09 10:37:07 -0400

    ENH: insert providers into separate table
    Also copy into us_covid via trigger function

--- src/cmdc_tools/datasets/covidtrackingproject/data.py
+++ src/cmdc_tools/datasets/covidtrackingproject/data.py
@@ -15,7 +15,7 @@ class CTP(InsertWithTempTable, DatasetBaseNoDate):
     by schema.sql
     """
 
-    table_name = "us_covid"
+    table_name = "ctp_covid"
     pk = '("vintage", "dt", "fips", "variable_id")'
     source = "https://covidtracking.com/"
 
@@ -36,11 +36,11 @@ class CTP(InsertWithTempTable, DatasetBaseNoDate):
 
     def _insert_query(self, df: pd.DataFrame, table_name: str, temp_name: str, pk: str):
         out = f"""
-        INSERT INTO data.us_covid (vintage, dt, fips, variable_id, value, provider)
-        SELECT tt.vintage, tt.dt, tt.fips, mv.id as variable_id, tt.value, 'ctp'
+        INSERT INTO data.{table_name} (vintage, dt, fips, variable_id, value)
+        SELECT tt.vintage, tt.dt, tt.fips, mv.id as variable_id, tt.value
         FROM {temp_name} tt
         LEFT JOIN meta.covid_variables mv ON tt.variable_name=mv.name
-        ON CONFLICT {pk} DO UPDATE SET value = excluded.value where excluded.provider < data.us_covid.provider;
+        ON CONFLICT {pk} DO UPDATE SET value = excluded.value
         """
 
         return textwrap.dedent(out)
