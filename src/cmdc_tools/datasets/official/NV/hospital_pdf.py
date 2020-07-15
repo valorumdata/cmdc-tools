6c14386a26094968d1bb25c95d6dc90de4a71efd
Chase Coleman <cc7768@gmail.com>
2020-07-15 17:50:58 -0400

    Figure out how to parse the pdf...

--- src/cmdc_tools/datasets/official/NV/hospital_pdf.py
+++ src/cmdc_tools/datasets/official/NV/hospital_pdf.py
@@ -8,40 +8,41 @@ from ...base import DatasetBaseNeedsDate
 from ..base import CountyData
 
 
-NVHA_keepcol = [
-    (False, "HospitalName"),
-    (False, "Region"),
-    (False, "Tier"),
-    (False, "Zip"),
-    (True, "county"),
-    (False, "CertNo"),
-    (True, "hospital_beds_in_use_any"),
-    (True, "hopsital_beds_capacity_count"),
-    (False, "hospital_beds_occ_rate"),
-    (True, "icu_beds_capacity_count"),
-    (True, "icu_beds_in_use_any"),
-    (False, "icu_beds_occ_rate"),
-    (True, "ventilators_capacity_count"),
-    (True, "ventilators_in_use_any"),
-    (False, "ventilators_occ_rate"),
-    (True, "hospital_beds_in_use_covid_confirmed"),
-    (True, "hospital_beds_in_use_covid_suspected"),
-    (False, "overflow_covid_total"),
-    (False, "overflow_on_vent_covid_total"),
-    (False, "hospital_beds_in_use_covid_new"),
-    (True, "icu_beds_in_use_covid_total"),
-    (True, "icu_beds_in_use_covid_new"),
-    (True, "ventilators_in_use_covid_total"),
-    (False, "ventilators_used_today"),
-    (False, "HFNC_in_use_covid_total"),
-    (False, "HFNC_used_today"),
-    (False, "patients_develop_covid_symptoms"),
-    (False, "covid_patient_deaths_total"),
-    (False, "covid_patient_deaths_new"),
-    (False, "staff_cases_total"),
-    (False, "staffing_levels"),
-    (False, "staffing_comments"),
-    (False, "N95_supply"),
+NVHA_keep_name_added = [
+    (False, "HospitalName", pd.to_datetime("2020-03-01")),
+    (False, "Region", pd.to_datetime("2020-03-01")),
+    (False, "Tier", pd.to_datetime("2020-03-01")),
+    (False, "Zip", pd.to_datetime("2020-03-01")),
+    (True, "county", pd.to_datetime("2020-03-01")),
+    (False, "CertNo", pd.to_datetime("2020-03-01")),
+    (True, "hospital_beds_capacity_count", pd.to_datetime("2020-03-01")),
+    (True, "hospital_beds_in_use_any", pd.to_datetime("2020-03-01")),
+    (False, "hospital_beds_occ_rate", pd.to_datetime("2020-03-01")),
+    (True, "icu_beds_capacity_count", pd.to_datetime("2020-03-01")),
+    (True, "icu_beds_in_use_any", pd.to_datetime("2020-03-01")),
+    (False, "icu_beds_occ_rate", pd.to_datetime("2020-03-01")),
+    (True, "ventilators_capacity_count", pd.to_datetime("2020-03-01")),
+    (True, "ventilators_in_use_any", pd.to_datetime("2020-03-01")),
+    (False, "ventilators_occ_rate", pd.to_datetime("2020-03-01")),
+    (True, "hospital_beds_in_use_covid_confirmed", pd.to_datetime("2020-03-01")),
+    (True, "hospital_beds_in_use_covid_suspected", pd.to_datetime("2020-03-01")),
+    (False, "overflow_covid_total", pd.to_datetime("2020-03-01")),
+    (False, "overflow_on_vent_covid_total", pd.to_datetime("2020-03-01")),
+    (False, "hospital_beds_in_use_covid_new", pd.to_datetime("2020-03-01")),
+    (False, "covid_patients_cleared_but_cannot_discharge", pd.to_datetime("2020-07-08")),
+    (True, "icu_beds_in_use_covid_total", pd.to_datetime("2020-03-01")),
+    (True, "icu_beds_in_use_covid_new", pd.to_datetime("2020-03-01")),
+    (True, "ventilators_in_use_covid_total", pd.to_datetime("2020-03-01")),
+    (False, "ventilators_used_today", pd.to_datetime("2020-03-01")),
+    (False, "HFNC_in_use_covid_total", pd.to_datetime("2020-03-01")),
+    (False, "HFNC_used_today", pd.to_datetime("2020-03-01")),
+    (False, "patients_develop_covid_symptoms", pd.to_datetime("2020-03-01")),
+    (False, "covid_patient_deaths_total", pd.to_datetime("2020-03-01")),
+    (False, "covid_patient_deaths_new", pd.to_datetime("2020-03-01")),
+    (False, "staff_cases_total", pd.to_datetime("2020-03-01")),
+    (False, "staffing_levels", pd.to_datetime("2020-03-01")),
+    (False, "staffing_comments", pd.to_datetime("2020-03-01")),
+    (False, "N95_supply", pd.to_datetime("2020-03-01")),
 ]
 
 
@@ -49,25 +50,73 @@ class NVHospitalPdf(DatasetBaseNeedsDate, CountyData):
     state_fips = int(us.states.lookup("Nevada").fips)
     has_fips = False
 
+    def determine_cols(self, dt, how="all"):
+        if how == "dt":
+            keep = lambda x: x[2] <= dt
+        elif how == "keep":
+            keep = lambda x: x[0]
+        else:
+            keep = lambda x: x[0] and x[2] <= dt
+
+        return [c[1] for c in NVHA_keep_name_added if keep(c)]
+
+    def get_date(self, pdf_file):
+        # Open file and extract text
+        f = fitz.open(pdf_file)
+        dt = f.getPageText(0).split("\n")[1]
+
+        return pd.to_datetime(dt).date()
+
     def parse_pdf(self, pdf_file):
-        # Read the two tables on the page
-        tables = camelot.read_pdf(pdf_file)
-        hospital_table = pd.concat(
-            [tables[0].df.iloc[2:, :-1], tables[1].df], ignore_index=True, axis=0
+        # Get the date
+        dt = self.get_date(pdf_file)
+
+        # Get the column names based on whether the column was active
+        # on given dt
+        cols = self.determine_cols(dt, how="dt")
+
+        # Read all tables on the first page
+        tables = camelot.read_pdf(
+            pdf_file, pages="1", flavor="lattice", shift_text="",
+            layout_kwargs={"char_margin": 0.5}
         )
-        hospital_table.columns = [el[1] for el in NVHA_keepcol]
+
+        rows = []
+        for table in tables:
+            _df = table.df
+            if len(_df.columns) != len(cols):
+                msg = "Column counts don't line up\n"
+                msg += "Check the columns in the pdf against code cols"
+                raise ValueError(msg)
+
+            _df.columns = cols
+            for row in _df.itertuples(index=False):
+                # Skip any rows with the wrong columns
+                if not str(row.Tier).isnumeric():
+                    continue
+                elif "hospital name" in row.HospitalName.lower():
+                    continue
+                elif "fail" in row.HospitalName.lower():
+                    continue
+
+                # If it makes it this far then add it to the rows
+                rows.append(row)
+        hospital_table = pd.DataFrame(rows)
 
         # For whatever reason, Horizon Specialty Hospital reports
         # 'NEVADA' as their county... Google search reveals that it
         # is in Clark county.
         # A few other counties report 'County' at the end of their
         # county name
+        # One hospital seems to sometimes report 'Carson' rather than
+        # 'Carson City'
         hospital_table["county"] = (
             hospital_table["county"]
             .str.normalize("NFKD")
             .str.title()
-            .replace("Nevada", "Clark")
             .str.replace(" County", "")
+            .replace("Nevada", "Clark")
+            .replace("Carson", "Carson City")
             .str.strip()
         )
         hospital_table = hospital_table.apply(
@@ -77,11 +126,15 @@ class NVHospitalPdf(DatasetBaseNeedsDate, CountyData):
         return hospital_table
 
     def get(self, pdf_file):
+        # Get date and DataFrame
+        dt = self.get_date(pdf_file)
         ht = self.parse_pdf(pdf_file)
 
         # Only keep columns that we'll use and create new total
         # covid patients
-        ht = ht.loc[:, [c for (k, c) in NVHA_keepcol if k]]
+        cols = self.determine_cols(dt, how="all")
+
+        ht = ht.loc[:, cols]
         ht["hospital_beds_in_use_covid_total"] = ht.eval(
             "hospital_beds_in_use_covid_confirmed + hospital_beds_in_use_covid_suspected"
         )
@@ -96,7 +149,3 @@ class NVHospitalPdf(DatasetBaseNeedsDate, CountyData):
         ).date()
 
         return ht
-
-
-if __name__ == "__main__":
-    pass
