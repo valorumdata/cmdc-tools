60116415a534dc2441e839809c0851227867c06e
Chase Coleman <cc7768@gmail.com>
2020-07-20 18:44:32 -0400

    Add HospitalBedsHospital scraper and misc changes

--- src/cmdc_tools/datasets/official/FL/__init__.py
+++ src/cmdc_tools/datasets/official/FL/__init__.py
@@ -11,26 +11,65 @@ import pandas as pd
 
 class FloridaHospitalUsage(TableauNeedsClick):
 
-    url = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/ICUBedsHospital?:isGuestRedirectFromVizportal=y&:embed=y"
+    url = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/HospitalBedsHospital?:isGuestRedirectFromVizportal=y&:embed=y"
 
     async def _pre_click(self, page: pyppeteer.page.Page):
         await page.waitForSelector(".tabCanvas")
 
-    def _clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        str_cols = ["County", "FileNumber", "ProviderName"]
+    def _clean_cols(self, df: pd.DataFrame, bads: list, str_cols: list) -> pd.DataFrame:
         for col in list(df):
-            for bad in (r",", r"%", "nan"):
+            for bad in bads:
                 df[col] = df[col].astype(str).str.replace(bad, "")
             if col not in str_cols:
                 df[col] = pd.to_numeric(df[col])
+
+        return df
+
+    def _clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        bads = [r",", r"%", "nan"]
+        str_cols = ["County", "FileNumber", "ProviderName"]
+        df = self._clean_cols(df, bads, str_cols)
+        df["county"] = df["County"].str.title()
+
+        # Rename appropriate columns
+        df = df.rename(
+            columns={
+                "Bed Census": "hospital_beds_in_use_any",
+                "Total Staffed Bed Capacity": "hospital_beds_capacity_count",
+            }
+        )
+
+        out_cols = [
+            "county",
+            "hospital_beds_in_use_any",
+            "hospital_beds_capacity_count",
+        ]
+        return (
+            df.query("county != 'Grand Total'")
+            .loc[:, out_cols]
+            .groupby("county")
+            .sum()
+            .reset_index()
+            .melt(id_vars=["county"], var_name="variable_name")
+        )
+
+
+class FloridaICUUsage(FloridaHospitalUsage):
+
+    url = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/ICUBedsHospital?:isGuestRedirectFromVizportal=y&:embed=y"
+
+    def _clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        bads = [r",", r"%", "nan"]
+        str_cols = ["County", "FileNumber", "ProviderName"]
+        df = self._clean_cols(df, bads, str_cols)
+        df["county"] = df["County"].str.title()
 
         df["icu_beds_in_use_any"] = df["Adult ICU Census"] + df["Pediatric ICU Census"]
         df["icu_beds_capacity_count"] = (
             df["Total AdultICU Capacity"] + df["Total PediatricICU Capacity"]
         )
         return (
-            df.rename(columns={"County": "county"})
-            .query("county != 'Grand Total'")
+            df.query("county != 'Grand Total'")
             .loc[:, ["county", "icu_beds_capacity_count", "icu_beds_in_use_any"]]
             .groupby("county")
             .sum()
@@ -54,23 +93,26 @@ class FloridaHospitalCovid(TableauNeedsClick):
 
 class FloridaHospital(DatasetBaseNoDate, CountyData):
     source = FloridaHospitalUsage.url
-    provider = "county"
     has_fips = False
     state_fips = int(us.states.lookup("Florida").fips)
 
     def get(self):
+        fiu = FloridaICUUsage()
         fhu = FloridaHospitalUsage()
         fhc = FloridaHospitalCovid()
-        today = pd.Timestamp.utcnow().normalize()
-        return pd.concat([fhu.get(), fhc.get()], ignore_index=True, sort=True).assign(
-            dt=today, vintage=today
+        today = (
+            pd.Timestamp.utcnow().tz_convert("US/Eastern").tz_localize(None).normalize()
         )
+        vintage = pd.Timestamp.utcnow().normalize()
+        return pd.concat(
+            [fiu.get(), fhu.get(), fhc.get()], ignore_index=True, sort=True
+        ).assign(dt=today, vintage=vintage)
 
 
 class Florida(DatasetBaseNoDate, ArcGIS):
     ARCGIS_ID = "CY1LXxl9zlJeBuRZ"
     state_fips = int(us.states.lookup("Florida").fips)
-    has_fips: bool = True
+    has_fips = True
     source = "https://fdoh.maps.arcgis.com/apps/opsdashboard/index.html#/8d0de33f260d444c852a615dc7837c86"
 
     def get(self):
