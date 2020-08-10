5c4ebaeda5a6fc0973d3b6050e0d0b583dd64053
Chase Coleman <cc7768@gmail.com>
2020-08-10 14:21:54 -0400

    Add Orange County, CA to inits

--- src/cmdc_tools/datasets/official/__init__.py
+++ src/cmdc_tools/datasets/official/__init__.py
@@ -2,7 +2,10 @@ from .AK import Alaska
 from .AL import AlabamaCounty, AlabamaFips
 from .AR import Arkansas
 from .base import ArcGIS, CountyData
-from .CA import CACountyData, California, Imperial, LosAngeles, SanDiego
+from .CA import (
+    CACountyData, California, Imperial, LosAngeles,
+    CAOrange, SanDiego
+)
 from .CT import ConnecticutCounty, ConnecticutState
 from .DC import DC
 from .DE import Delaware, DelawareKent, DelawareNewCastle, DelawareSussex
