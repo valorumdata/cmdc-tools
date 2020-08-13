22e0b394dea7c0a3728406ec316cec65916a9d9d
Trevor Lyon <trevor.lyon@icloud.com>
2020-08-13 10:58:06 -0400

    get hosp data

--- src/cmdc_tools/datasets/official/NV/data.py
+++ src/cmdc_tools/datasets/official/NV/data.py
@@ -17,13 +17,17 @@ class NevadaFips(DatasetBaseNoDate):
     def get(self):
         cases = self._get_cases()
         tests = self._get_tests()
-        return pd.concat([tests, cases], sort=False)
+        hosp = self._get_hosp()
+        return pd.concat([tests, cases, hosp], sort=False)
 
     def _get_tests(self):
         return asyncio.run(self._get_tests_async())
 
     def _get_cases(self):
         return asyncio.run(self._get_cases_async())
+
+    def _get_hosp(self):
+        return asyncio.run(self._get_hosp_async())
 
     async def _get_tests_async(self):
         async with with_page() as page:
@@ -78,7 +82,7 @@ class NevadaFips(DatasetBaseNoDate):
         elems = await visual_modern.Jx(
             "//*[@class='series']//*[@class='column setFocusRing']"
         )
-
+        # print("elems\n", elems)
         labels = [
             (await page.evaluate("(el) => el.getAttribute('aria-label')", e))
             for e in elems
@@ -135,6 +139,82 @@ class NevadaFips(DatasetBaseNoDate):
             return renamed.melt(id_vars=["dt"], var_name="variable_name").assign(
                 vintage=pd.Timestamp.utcnow(), fips=self.state_fips
             )
+
+    async def _get_hosp_async(self):
+        async with with_page() as page:
+            await page.goto(self.source)
+            # Wait for dashboard to load
+            await page.waitForXPath("//span[text()='COVID-19 ']")
+            # Get next page button
+            button = await _get_next_page_button(page)
+            await button.click()
+            # Wait for dashboard to load (2)
+            await page.waitForXPath("//div[text()='COVID-19 Statistics by County']")
+            # Go to next page
+            await button.click()
+            # Wait for dashboard to laod (3)
+            await page.waitForXPath("//div[text()='Results Filter for Demographics']")
+            # Go to next page
+            await button.click()
+            # Wait for dashboard to laod (3)
+            await page.waitForXPath("//div[text()='Cumulative Tests Reported']")
+            # Go to next page
+            await button.click()
+            # Wait for dashboard to laod (3)
+            await page.waitForXPath("//div[text()='Cumulative Cases']")
+            # Go to next page
+            await button.click()
+            # Wait for dashboard to laod (3)
+            await page.waitForXPath("//div[text()='Cumulative Deaths']")
+            # Go to next page
+            await button.click()
+            # Wait for dashboard to laod (3)
+            await page.waitForXPath(
+                "//div[text()='Daily Growth Rate, Cases (April 1st to Current)']"
+            )
+            # Go to next page
+            await button.click()
+            # Find graph
+            graph = await page.waitForXPath("//*[@class='cartesianChart']")
+            # return graph
+            # all_labels = []
+            # labels = await self._get_labels_from_single_graph(page, graph[0])
+            # icu_labels = await self._get_labels_from_single_graph(page, graph[1])
+
+            await graph.click(button="right")
+            # Get table button
+            table_button = await page.waitForXPath("//h6[text()='Show as a table']")
+            await table_button.click()
+
+            labels = await self._get_labels_from_graph(page)
+            # parse labels
+            data = {"Suspected": [], "Confirmed": []}
+            for label in labels:
+                split = label.split(". ")
+                date = split[0].split("Date")[1].strip()
+                tests = split[1].split(" ")
+                tests_type = tests[0].strip()
+                # Skip all new tests
+
+                tests_num = int(tests[1][:-1].strip().replace(",", ""))
+                {"Date": date, f"{tests_type}": tests_num}
+
+                # data.append({"Date": date, f"{tests_type}": tests_num})
+                data[tests_type].append({"dt": date, "value": tests_num})
+            suspected = pd.DataFrame(data["Suspected"]).assign(
+                variable_name="hospital_beds_in_use_covid_suspected"
+            )
+            confirmed = pd.DataFrame(data["Confirmed"]).assign(
+                variable_name="hospital_beds_in_use_covid_confirmed"
+            )
+
+            df = pd.concat([suspected, confirmed])
+
+            df.dt = pd.to_datetime(df.dt)
+            return df.assign(vintage=pd.Timestamp.utcnow(), fips=self.state_fips)
+
+    def _parse_hospitalizations(self, labels):
+        pass
 
 
 class NevadaCounty(DatasetBaseNoDate):
