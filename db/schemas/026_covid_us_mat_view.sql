57ee3169c8646663a0e96d7c95799a52f2cde4c9
Spencer Lyon <spencerlyon2@gmail.com>
2020-08-25 12:34:35 -0400

    ENH: add fips column as zero padded text in covid_us view

--- db/schemas/026_covid_us_mat_view.sql
+++ db/schemas/026_covid_us_mat_view.sql
@@ -12,7 +12,11 @@ CREATE OR REPLACE VIEW api.covid_us AS
  SELECT uc.dt,
     uc.fips AS location,
     cv.name AS variable,
-    uc.value
+    uc.value,
+    CASE WHEN fips < 100 THEN LPAD(fips::TEXT, 2, '0') ELSE LPAD(fips::TEXT, 5, '0') END AS fips
    FROM data.us_covid_last_vintage lv
      LEFT JOIN data.us_covid uc USING (dt, fips, variable_id, vintage)
      LEFT JOIN meta.covid_variables cv ON cv.id = uc.variable_id;
+
+COMMENT ON COLUMN api.covid_us.location IS E'An integer identifying the geography. For US states and counties this is a fips code without leading zeros.';
+COMMENT ON COLUMN api.covid_us.location IS E'The 2 digit fips code for US states and 5 digit fips code for US counties.';
