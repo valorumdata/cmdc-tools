5bbbfe05ec268536c95f64463f7842494da27230
Spencer Lyon <spencerlyon2@gmail.com>
2020-09-02 14:23:45 -0400

    fix owid scraper

---
+++ db/schemas/029_owid_new_variables.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "data"."owid_covid" ADD COLUMN "new_cases_smoothed" float4;
+ALTER TABLE "data"."owid_covid" ADD COLUMN "new_cases_smoothed_per_million" float4;
+ALTER TABLE "data"."owid_covid" ADD COLUMN "new_deaths_smoothed" float4;
+ALTER TABLE "data"."owid_covid" ADD COLUMN "new_deaths_smoothed_per_million" float4;
