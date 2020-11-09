fca4ab6043735f9b03fc0c82076b6e8418dec308
Sarah Colquhoun <38871276+sarah-colq@users.noreply.github.com>
2020-11-09 13:41:55 -0500

    Add test positivity to Hawaii scraper. Update hawaii scraper to use h… (#137)
    * Add test positivity to Hawaii scraper. Update hawaii scraper to use hiema site.
    
    * Correct variables from positivity rate to total tests and positive tests.
    
    * Update src/cmdc_tools/datasets/official/HI/data.py
    
    Co-authored-by: Chase Coleman <cc7768@gmail.com>

--- src/cmdc_tools/datasets/official/HI/data.py
+++ src/cmdc_tools/datasets/official/HI/data.py
@@ -7,27 +7,51 @@ from ..base import ArcGIS
 
 class Hawaii(DatasetBaseNoDate, ArcGIS):
 
-    ARCGIS_ID = "HQ0xoN0EzDPBOEci"
+    ARCGIS_ID = "aKxrz4vDVjfUwBWJ"
     state_fips = int(us.states.lookup("Hawaii").fips)
     has_fips = False
-    source = "https://experience.arcgis.com/experience/eb56a98b71324152a918e72d3ccdfc20"
+    source = "https://hiema-hub.hawaii.gov/"
 
     def get(self):
-        df = self.get_all_sheet_to_df("covid_county_counts", 0, "")
+        cases = self._get_cases()
+        testing = self._get_testing()
+
+        df = pd.concat([testing, cases], axis=0, ignore_index=True).sort_values(["dt", "county"])
+        df["vintage"] = self._retrieve_vintage()
+
+        return df
+
+
+    def _get_cases(self):
+        df = self.get_all_sheet_to_df("HIEMA_COVID_CASES_PUBLIC_LATEST", 0, "9")
         renamed = df.rename(
             columns={
-                "cases": "cases_total",
-                # "hosp": "hospitalized_total"
-                "deaths": "deaths_total",
-                "NAME10": "county",
+                "name": "county",
+                "toDate_positiveAndPresumed": "cases_total",
+                "toDate_fatalities": "deaths_total"
             }
         )
         keep_rows = ["Hawaii", "Honolulu", "Kauai", "Maui"]
         renamed["dt"] = self._retrieve_dt("US/Hawaii")
         renamed = renamed.loc[renamed.county.isin(keep_rows)]
-
         return (
             renamed[["county", "dt", "cases_total", "deaths_total"]]
             .melt(id_vars=["dt", "county"], var_name="variable_name")
-            .assign(vintage=self._retrieve_vintage())
+        )
+
+    def _get_testing(self):
+        df = self.get_all_sheet_to_df("HIEMA_TEST_DATA_PUBLIC_LATEST", 0, "9")
+        renamed = df.rename(
+            columns={
+                "name": "county",
+                "toDate_totaltests": "tests_total",
+                "toDate_positive": "positive_tests_total"
+            }
+        )
+        keep_rows = ["Hawaii", "Honolulu", "Kauai", "Maui"]
+        renamed["dt"] = self._retrieve_dt("US/Hawaii")
+        renamed = renamed.loc[renamed.county.isin(keep_rows)]
+        return (
+            renamed[["county", "dt", "tests_total", "positive_tests_total"]]
+            .melt(id_vars=["dt", "county"], var_name="variable_name")
         )
