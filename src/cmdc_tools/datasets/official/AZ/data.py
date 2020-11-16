587e5dde9fa52d1709b82942068354a11b8f8bed
Sarah Colquhoun <scolquho@andrew.cmu.edu>
2020-11-15 19:53:57 -0500

    Get test data and cases data from Arizona dashboard.

--- src/cmdc_tools/datasets/official/AZ/data.py
+++ src/cmdc_tools/datasets/official/AZ/data.py
@@ -6,96 +6,117 @@ import pandas as pd
 
 from ...base import DatasetBaseNoDate
 
-def onAlias(it, value, cstring):
-    return value[it] if (it >= 0) else cstring["dataValues"][abs(it)-1]
 
 class Arizona(DatasetBaseNoDate):
+
+    base_url = 'https://tableau.azdhs.gov'
+
     def get(self):
-        with requests.Session() as req:
-            # Find the URL of the data.
-            resp = req.get("https://tableau.azdhs.gov/views/ELRcovid/LaboratoryTestingExternalDraft?:isGuestRedirectFromVizportal=y&:embed=y")
-            soup = BeautifulSoup(resp.text, "html.parser")
-            tableau_tag = soup.find("textarea", {"id": "tsConfigContainer"})
-            tableau_data = json.loads(tableau_tag.text)
-            data_url = f'https://tableau.azdhs.gov/{tableau_data["vizql_root"]}/bootstrapSession/sessions/{tableau_data["sessionid"]}'
-            
-            # Request the data.
-            resp = requests.post(data_url, data= {"sheet_id": tableau_data["sheetId"],})
+        testing = self._get_testing()
+        cases = self._get_cases()
+        
+        df = pd.concat([testing, cases], axis=0).sort_values(["dt", "county"])
+        df["vintage"] = self._retrieve_vintage()
 
-            # Parse the response.
-            # The response contains multiple chuncks of the form
-            # `<size>;<json>` where `<size>` is the number of bytes in `<json>`
-            resp_text = resp.text
-            data = []
-            while len(resp_text) != 0:
-                size, rest = resp_text.split(";", 1)
-                chunck = json.loads(rest[:int(size)])
-                data.append(chunck)
-                resp_text = rest[int(size):]
-            
+        return df
 
-            tableau_data = data[0]["worldUpdate"]["applicationPresModel"]["workbookPresModel"]["dashboardPresModel"]
+    def _get_cases(self):
+        viewPath = 'EpiCurve/EpiCurveDashboard'
+        data = self._scrape_view(viewPath)
+        df = data['Map']
+        renamed = df.rename(
+            columns={
+            "Countynm-value": "county",
+            "SUM(Number of Records)-alias": "cases_total"
+        })
+        renamed["dt"] = self._retrieve_dt("US/Mountain")
+        return (
+            renamed[["county", "dt", "cases_total"]]
+            .melt(id_vars=["dt", "county"], var_name="variable_name")
+        ) 
 
-            views = tableau_data["viewIds"]
-            zones = tableau_data["zones"]
+    def _get_testing(self):
+        viewPath = 'ELRcovid/LaboratoryTestingExternalDraft'
+        data = self._scrape_view(viewPath)
+        df = data['Map of Arizona']
+        renamed = df.rename(
+            columns={
+            "County (pt if avail, or provider))-value": "county",
+            "SUM(Number of Records)-alias": "positive_tests_total"
+        })
+        renamed["county"] = renamed["county"].apply(lambda x: x.lower().capitalize())
+        renamed["dt"] = self._retrieve_dt("US/Mountain")
+        return (
+            renamed[["county", "dt", "positive_tests_total"]]
+            .melt(id_vars=["dt", "county"], var_name="variable_name")
+        )           
 
-            '''
-            for key, zone in zones.items():
-                if ('presModelHolder' in zone):
-                    #print('\n\n')
-                    if 'worksheet' in zone:
-                        print(zone['worksheet'])
+    def _scrape_view(self, viewPath):
 
-                    else:
-                        print('NO WORKSHEET')
-                        print(zone)                
-                     #(zone['worksheet'] == 'Total Tests Conducted') and 
-                    if ('visual' in zone['presModelHolder']):
-                        for k, v in zone['presModelHolder']['visual'].items():
-                            print(k)
-                            print(v)
-                        if 'scene' in zone['presModelHolder']['visual']:
-                            secondBottom = zone['presModelHolder']['visual']['scene']
-                            if 'runtimeRenderInputDatastore' in secondBottom:
-                                hexText = secondBottom['runtimeRenderInputDatastore']
-                                #print(' '.join([hexText[i:i+2] for i in range(0, len(hexText), 2)]))
-                                #print(' '.join([hexText[i:i+4] for i in range(0, len(hexText), 4)]))
-                                #print(bytearray.fromhex(hexText).decode('ascii'))
-                                #print(bytes.fromhex(hexText).decode('utf-8'))
-            '''
-            
-            valueInfo = data[1]['secondaryInfo']['presModelMap']['vizData']['presModelHolder']['genPresModelMapPresModel']['presModelMap']
-            dataFull = data[1]["secondaryInfo"]["presModelMap"]["dataDictionary"]["presModelHolder"]["genDataDictionaryPresModel"]["dataSegments"]["0"]["dataColumns"]
+        def onAlias(it, value, cstring):
+            return value[it] if (it >= 0) else cstring["dataValues"][abs(it)-1] 
 
-            for metric in valueInfo:
-                print('\n')
-                print(metric)
-                metricsDict = valueInfo[metric]['presModelHolder']['genVizDataPresModel']
+        req = requests.Session()
+        fullURL = self.base_url + '/views/' + viewPath
+        reqg = req.get(fullURL, 
+                    params= {":isGuestRedirectFromVizportal" : "y",
+                            ":embed" : "y"}
+                                )
+        soup = BeautifulSoup(reqg.text, "html.parser")
+        tableauTag = soup.find("textarea", {"id": "tsConfigContainer"})
+        tableauData = json.loads(tableauTag.text)
+        dataUrl = f'{self.base_url}/{tableauData["vizql_root"]}/bootstrapSession/sessions/{tableauData["sessionid"]}'
 
-                columnsData = metricsDict['paneColumnsData'] #['vizDataColumns']
-                result = [ {
-                        "fieldCaption": t.get("fieldCaption", ""), 
-                        "valueIndices": columnsData["paneColumnsList"][t["paneIndices"][0]]["vizPaneColumns"][t["columnIndices"][0]]["valueIndices"],
-                        "aliasIndices": columnsData["paneColumnsList"][t["paneIndices"][0]]["vizPaneColumns"][t["columnIndices"][0]]["aliasIndices"],
-                        "dataType": t.get("dataType"),
-                        "paneIndices": t["paneIndices"][0],
-                        "columnIndices": t["columnIndices"][0]
-                        }
-                        for t in columnsData["vizDataColumns"]
-                        if t.get("fieldCaption")
-                ]
-                frameData = {}
-                cstring = [t for t in dataFull if t["dataType"] == "cstring"][0]
-                for t in dataFull:
-                    for index in result:
-                        if (t["dataType"] == index["dataType"]):
-                            if len(index["valueIndices"]) > 0:
-                                frameData[f'{index["fieldCaption"]}-value'] = [t["dataValues"][abs(it)] for it in index["valueIndices"]]
-                            if len(index["aliasIndices"]) > 0:
-                                frameData[f'{index["fieldCaption"]}-alias'] = [onAlias(it, t["dataValues"], cstring) for it in index["aliasIndices"]]
+        resp = requests.post(dataUrl, data= {"sheet_id": tableauData["sheetId"],})
+        # Parse the response.
+        # The response contains multiple chuncks of the form
+        # `<size>;<json>` where `<size>` is the number of bytes in `<json>`
+        resp_text = resp.text
+        data = []
+        while len(resp_text) != 0:
+            size, rest = resp_text.split(";", 1)
+            chunck = json.loads(rest[:int(size)])
+            data.append(chunck)
+            resp_text = rest[int(size):]
 
-                df = pd.DataFrame.from_dict(frameData, orient='index').fillna(0).T
-                with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
-                    print(df)
-                
-            return data
+        presModel = data[1]['secondaryInfo']['presModelMap']
+        metricInfo = presModel['vizData']['presModelHolder']
+        metricInfo = metricInfo['genPresModelMapPresModel']['presModelMap']
+        data = presModel["dataDictionary"]["presModelHolder"]
+        data = data["genDataDictionaryPresModel"]["dataSegments"]["0"]["dataColumns"]
+
+        scrapedData = {}
+
+        for metric in metricInfo:
+            metricsDict = metricInfo[metric]['presModelHolder']['genVizDataPresModel']
+            columnsData = metricsDict['paneColumnsData']
+
+            result = [ {
+                    "fieldCaption": t.get("fieldCaption", ""), 
+                    "valueIndices": columnsData["paneColumnsList"][t["paneIndices"][0]]["vizPaneColumns"][t["columnIndices"][0]]["valueIndices"],
+                    "aliasIndices": columnsData["paneColumnsList"][t["paneIndices"][0]]["vizPaneColumns"][t["columnIndices"][0]]["aliasIndices"],
+                    "dataType": t.get("dataType"),
+                    "paneIndices": t["paneIndices"][0],
+                    "columnIndices": t["columnIndices"][0]
+                    }
+                    for t in columnsData["vizDataColumns"]
+                    if t.get("fieldCaption")
+            ]
+            frameData = {}
+            cstring = [t for t in data if t["dataType"] == "cstring"][0]
+            for t in data:
+                for index in result:
+                    if (t["dataType"] == index["dataType"]):
+                        if len(index["valueIndices"]) > 0:
+                            frameData[f'{index["fieldCaption"]}-value'] = [t["dataValues"][abs(it)] for it in index["valueIndices"]]
+                        if len(index["aliasIndices"]) > 0:
+                            frameData[f'{index["fieldCaption"]}-alias'] = [onAlias(it, t["dataValues"], cstring) for it in index["aliasIndices"]]
+
+            df = pd.DataFrame.from_dict(frameData, orient='index').fillna(0).T
+
+            scrapedData[metric] = df
+
+        return scrapedData
+
+
+        
